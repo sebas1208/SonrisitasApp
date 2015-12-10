70b9ae28feb1514c5c7f8d771f0a0336fc3544fd
Sebastian Avalos <sebas1208.avalos@gmail.com>
2015-12-10 17:09:44 -0500

    Añadida todas las rutas para los servicios rest

--- app/controllers/restServices/AdministradorController.java
+++ app/controllers/restServices/AdministradorController.java
@@ -26,45 +26,9 @@ public class AdministradorController extends Controller {
         return Results.ok(Json.toJson(administradores));
     }
 
-/*    @Transactional
-    public Result one(String id) {
-        Odontologo odontologo = JPA.em().find(Odontologo.class, Integer.parseInt(id));
-        if(odontologo == null) return Results.ok("{\nerror: \"No hay datos\n\"}");
-        return Results.ok(Json.toJson(odontologo));
-    }*/
-/*    @BodyParser.Of(BodyParser.Json.class)
-    public Result submitOdontologo() {
-    	JsonNode jsonNode = Controller.request().body().asJson();
-        String url = jsonNode.findPath("url").asText();
-        String fullname = jsonNode.findPath("fullname").asText();
-        JsonNode response = fetchInformation(url);
-        Odontologo odontologo = null;
-        if(response == null){
-            return Results.badRequest();
-        }else{
-            String odoCedula = response.findPath("cedula").textValue();
-            String odoNombres = response.findPath("nombres").textValue();
-            String odoApellidos = response.findPath("apellidos").textValue();
-            String odoDireccion = response.findPath("direccion").textValue();
-            String odoTelefono = response.findPath("telefono").textValue();
-            String odoEmail = response.findPath("email").textValue();
-            odontologo = new Odontologo(odoCedula, odoNombres, odoApellidos, odoDireccion, odoTelefono, odoEmail);
-        }
-        odontologo.save();
-        return Results.created();
-    }
-
-    private static JsonNode fetchInformation(String url){
-        String restServiceUrl = "http://localhost/api/v1/extract?url="+url;
-        Future<Response> future = WS.url(restServiceUrl).get();
-        try {
-            Response result = Await.result(future, Duration.apply(30, TimeUnit.SECONDS));
-            JsonNode jsonNode = Json.parse(result.json().toString());
-            return jsonNode;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-
+/*    public Result one(Long id){
+        Administrador administrador = Administrador.find.byId(id);
+        if(administrador == null) return Results.ok("{\"error\":\"No existe el usuario\"}");
+        return Results.ok(Json.toJson(usuario));
     }*/
 }
